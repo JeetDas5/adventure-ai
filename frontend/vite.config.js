ce40e042056a3fb5806f48e7c697b945e634228a
JeetDas5 <jeet15083011@gmail.com>
2026-05-04 13:06:03 +0530

    finalisation

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -3,16 +3,22 @@ import react from "@vitejs/plugin-react";
 
 // https://vite.dev/config/
 export default defineConfig((command, mode) => {
+  const env = loadEnv(mode, process.cwd(), "");
+
+  console.log(env.VITE_DEBUG);
+
   return {
     plugins: [react()],
     server: {
-      proxy: {
-        "/api": {
-          target: "http://localhost:8000",
-          changeOrigin: true,
-          secure: false,
+      ...(env.VITE_DEBUG === "true" && {
+        proxy: {
+          "/api": {
+            target: "http://localhost:8000",
+            changeOrigin: true,
+            secure: false,
+          },
         },
-      },
+      }),
     },
   };
 });
